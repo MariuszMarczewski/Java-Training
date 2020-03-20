03194f8030d10033ec780671972fb7313d79a6b8
Mariusz Marczewski <mariusz.marczewski1@poczta.fm>
2020-03-20 16:47:24 +0100

    Phones done

---
+++ src/Phones/ITelephone.java
@@ -0,0 +1,11 @@
+package Phones;
+
+
+public interface ITelephone {
+    void powerOn();
+    void dial(int phoneNumber);
+    void answer();
+    boolean callPhone(int phoneNumber);
+    boolean isRinging();
+}
+
